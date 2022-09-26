46f5532d48aef30565509f57b01a0bdb1fefac28
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-09-26 08:40:45 -0300

    Requirement seven

---
+++ desafio7.sql
@@ -0,0 +1,11 @@
+SELECT
+si.singer_name AS artista,
+al.album_name AS album,
+COUNT(fo.user_id) AS seguidores
+FROM SpotifyClone.singers AS si
+INNER JOIN SpotifyClone.albums AS al
+ON si.singer_id = al.singer_id
+INNER JOIN SpotifyClone.following AS fo
+ON al.singer_id = fo.singer_id
+GROUP BY album, artista
+ORDER BY seguidores DESC, artista, album;
