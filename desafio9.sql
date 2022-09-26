488a47342958bd9a8405366897a45815cdb99fdc
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-09-26 10:43:21 -0300

    Requirement nine

---
+++ desafio9.sql
@@ -0,0 +1,6 @@
+SELECT 
+COUNT(*) AS quantidade_musicas_no_historico
+FROM SpotifyClone.users AS us
+INNER JOIN SpotifyClone.history AS hi
+ON us.user_id = hi.user_id
+WHERE us.user_name = 'Barbara Liskov';
