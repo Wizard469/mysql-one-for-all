51daac8bb062287c912a7150d77d744722ba04f3
Jonathan Rodrigues <jonathan.wiz@outlook.com>
2022-09-26 08:15:56 -0300

    Requirement six

---
+++ desafio6.sql
@@ -0,0 +1,8 @@
+SELECT
+MIN(pl.plan_price) AS faturamento_minimo,
+MAX(pl.plan_price) AS faturamento_maximo,
+ROUND(AVG(pl.plan_price), 2) AS faturamento_medio,
+SUM(pl.plan_price) AS faturamento_total
+FROM SpotifyClone.plans AS pl
+INNER JOIN SpotifyClone.users AS us
+ON us.plan_id = pl.plan_id;
